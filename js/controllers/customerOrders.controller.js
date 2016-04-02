211ca7efab5b82dcda1c416046aa0b58d73172ce
Ronnie Nyaga <ronnienyaga@gmail.com>
2016-04-03 02:55:15 +0300

    Add new order for customer

--- js/controllers/customerOrders.controller.js
+++ js/controllers/customerOrders.controller.js
@@ -6,13 +6,21 @@
         .module('customersApp')
         .controller('customerOrdersController', function($scope, $http, $stateParams, _) {
           var customerId = $stateParams.id.toString();
+          $scope.order = {};
+          $scope.orders = {};
           $http
             .get('customers/customers.json')
             .success(function(data) {
                 $scope.customer = _.where(data.customers, {id:customerId} );
                 $scope.names = $scope.customer[0].names;
                 $scope.orders = $scope.customer[0].orders;
-                console.log($scope.orders);
+                $scope.createCustomerOrder = function(){
+                  var order = $scope.order;
+                  $scope.order.thedate = Date.now();
+                  console.log(order);
+                  $scope.orders.push(order);
+                  $scope.order = "";
+                }
             });
           });
 })();
