6d37be6f71ab5bf9f1446c9aaf391fd95aaba313
Ronnie Nyaga <ronnienyaga@gmail.com>
2016-04-03 12:45:55 +0300

    Sorted orders by column name

--- js/controllers/customerOrders.controller.js
+++ js/controllers/customerOrders.controller.js
@@ -8,6 +8,19 @@
           var customerId = $stateParams.id.toString();
           $scope.order = {};
           $scope.orders = {};
+          $scope.rowLimit = 5;
+          $scope.sortColumn = "item";
+          $scope.reverseSort = false;
+          $scope.sortData = function(column){
+            $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false
+            $scope.sortColumn = column;
+          }
+          $scope.getSortClass = function(column){
+            if($scope.sortColumn == column){
+              return $scope.reverseSort ? 'arrow-down' : 'arrow-up'
+            }
+            return '';
+          }
           $http
             .get('customers/customers.json')
             .success(function(data) {
